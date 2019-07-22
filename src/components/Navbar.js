d504e5081b7b1f57dfe26e751ffc40229678cc23
Keith <ozmaneku@gmail.com>
2019-07-21 21:28:07 -0400

    Initial commit, begun work on nav bar

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class navbar extends Component {
+    render() {
+        return (
+            <div>
+                <ul>
+                    <li>About</li>
+                    <li>Live</li>
+                    <li>Merch</li>
+                    <li>Social</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default navbar;
